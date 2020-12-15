bf8c949386fea999172be8498323bb89f4ea01db
Meeyzt <mehmetbs@hotmail.com.tr>
2020-12-15 18:27:19 +0300

    Controllers

--- Foodly_new/Controllers/HomeController.cs
+++ Foodly_new/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Foodly_new.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Foodly_new.Models;
+using System.Diagnostics;
 
 namespace Foodly_new.Controllers
 {
