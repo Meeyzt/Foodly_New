91d4389954862b2f406fefc8af0479224399fa10
Meeyzt <mehmetbs@hotmail.com.tr>
2020-12-18 16:10:29 +0300

    EditorPage Configuration

--- Foodly_new/Controllers/BlogsController.cs
+++ Foodly_new/Controllers/BlogsController.cs
@@ -4,11 +4,11 @@ using Foodly_new.Models.EfModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PagedList.Core;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Foodly_new.Controllers
@@ -24,77 +24,6 @@ namespace Foodly_new.Controllers
             _userManager = userManager;
         }
 
-        EFContext c = new EFContext();
-
-        public IActionResult Index(int? id)
-        {
-            List<Review> x = c.Reviews.Where(i => i.Publish == true).ToList();
-            List<Review> y = new List<Review>();
-            UserIdentity user = new UserIdentity();
-            try
-            {
-                if (id == null || id == 0 || id == 1)
-                {
-                    if (x.Count <= 6)
-                    {
-                        y.Clear();
-                        for (int i = 0; i < x.Count; i++)
-                        {
-                       
-                        }
-                    }
-                    else
-                    {
-                        y.Clear();
-                        for (int i = 0; i < 6; i++)
-                        {
-                           
-                        }
-                    }
-
-                    return View(y);
-                }
-                else
-                {
-                    y.Clear();
-                    if (id >= 2)
-                    {
-                        int count = (int)(id * 6);
-                        int start = count - 6;
-
-                        if (count >= x.Count())
-                        {
-                            for (int i = start; i < x.Count(); i++)
-                            {
-                                
-                            }
-                        }
-                        else
-                        {
-                            for (int i = start; i <= count; i++)
-                            {
-                                
-                            }
-                        }
-                        return View(y);
-                    }
-                    else
-                    {
-                        return View(nameof(Index));
-                    }
-
-
-                }
-            }
-            catch (Exception e)
-            {
-                y.Clear();
-                for (int i = 0; i <= 6; i++)
-                {
-                    
-                }
-                return View(y);
-            }
         public IActionResult Index(int page = 1,int pageSize=6)
         {
             PagedList<Review> model = new PagedList<Review>(c.Reviews.Where(x=>x.Publish==true&&x.IsDeleted==false), page, pageSize);
@@ -112,7 +41,6 @@ namespace Foodly_new.Controllers
                 try
                 {
                     var blogContext = c.Reviews.Find(id);
-                    //username ekleenecek
                     var se = await _userManager.FindByIdAsync(blogContext.UserID);
                     ViewData["blogContent"] = blogContext.Blog.ToString();
                     ViewData["BlogHeader"] = blogContext.Header;
@@ -132,6 +60,66 @@ namespace Foodly_new.Controllers
                     return RedirectToAction(nameof(Index));
                 }
             }
+        }
+        [HttpPost]
+        public IActionResult Blog(string ReviewID, string Header)
+        {
+            try
+            {
+                if (Header == "Publish")
+                {
+                    if (ReviewID != null)
+                    {
+                        List<Review> r = new List<Review>();
+                        r = c.Reviews.Where(x => x.ReviewID == ReviewID && x.IsDeleted == false).ToList<Review>();
+                        foreach (var item in r)
+                        {
+                            item.Publish = true;
+                            item.IsDeleted = false;
+                            c.Update(item);
+                            c.SaveChanges();
+                        }
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ViewData["Error"] = "Bir hata oluştu #3303";
+                        return View();
+                    }
+                }
+                else if (Header == "Delete")
+                {
+                    if (ReviewID != null)
+                    {
+                        List<Review> r = new List<Review>();
+                        r = c.Reviews.Where(x => x.ReviewID == ReviewID && x.IsDeleted == false).ToList<Review>();
+                        foreach (var item in r)
+                        {
+                            item.Publish = false;
+                            item.IsDeleted = true;
+                            c.Update(item);
+                            c.SaveChanges();
+                        }
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ViewData["Error"] = "Bir hata oluştu #3303";
+                        return View();
+                    }
+                }
+                else
+                {
+                    ViewData["Error"] = "Bir hata oluştu #3303";
+                    return View();
+                }
+            }
+            catch
+            {
+                ViewData["Error"] = "Bir hata oluştu #3303";
+                return View();
+            }
+
         }
 
         [HttpGet]
